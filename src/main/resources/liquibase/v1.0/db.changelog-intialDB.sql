f099daf10d01834d1126c3fea47262249f6a5571
zoom59rus <anton_1@bk.ru>
2021-01-24 20:48:23 +0500

    Created database with Liquibase VCS support

---
+++ src/main/resources/liquibase/v1.0/db.changelog-intialDB.sql
@@ -0,0 +1,34 @@
+--liquibase formatted sql
+--changeset nazarov:1
+
+CREATE TABLE regions
+(
+    id   INT         NOT NULL AUTO_INCREMENT UNIQUE,
+    name varchar(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE writers
+(
+    id         BIGINT      NOT NULL AUTO_INCREMENT UNIQUE,
+    regions_id INT,
+    firstName  varchar(50) NOT NULL,
+    lastName   varchar(50) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_writers_regions
+        FOREIGN KEY (regions_id) REFERENCES regions (id)
+            ON DELETE SET NULL
+);
+
+CREATE TABLE posts
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT UNIQUE,
+    writers_id BIGINT       NOT NULL,
+    content    varchar(255) NOT NULL,
+    `create`   DATETIME,
+    `upgrade`  DATETIME,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_posts_writers
+        FOREIGN KEY (writers_id) REFERENCES writers (id)
+            ON DELETE CASCADE
+);
